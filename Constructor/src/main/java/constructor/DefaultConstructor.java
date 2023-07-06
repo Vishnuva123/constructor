c647e47dddd94fddbc83d26f0e61f58e0b6aca29
Klince Sebin Pious <Klince Sebin Pious@DESKTOP-7437I6P>
2023-07-06 20:08:17 +0530

    default constructor

---
+++ Constructor/src/main/java/constructor/DefaultConstructor.java
@@ -0,0 +1,18 @@
+package constructor;
+
+public class DefaultConstructor 
+{
+	public DefaultConstructor()
+	{
+	int a=20;
+	String n="vishnu";
+	System.out.println(" id:  "+a);
+	System.out.println("  name:  "+n);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DefaultConstructor add=new DefaultConstructor();
+
+	}
+
+}
